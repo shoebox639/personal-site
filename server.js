19a6310f3808ae66bd07bfd71ffb302505f5675a
Shu Song <shoebox@gmail.com>
2016-04-04 00:55:14 -0400

    converting server to es6

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-var express = require('express');
-var compression = require('compression')
+const express = require('express');
+const compression = require('compression');
 
-var app = express();
+const app = express();
 
 app.use(compression());
 
-app.use('/assets/node_modules', express.static(__dirname + '/node_modules'));
-app.use('/assets/bower_components', express.static(__dirname + '/bower_components'));
-app.use('/assets', express.static(__dirname + '/build'));
+app.use('/assets/node_modules', express.static(`{__dirname}/node_modules`));
+app.use('/assets/bower_components', express.static(`{__dirname}/bower_components`));
+app.use('/assets', express.static(`{__dirname}/build`));
 
-var validPaths = ['about', 'resume', 'contact'];
+const validPaths = ['about', 'resume', 'contact'];
 
-app.get('/:path?', function(req, res, next) {
-  var path = req.params.path;
+app.get('/:path?', (req, res, next) => {
+  const path = req.params.path;
   if (!path || validPaths.indexOf(path) > -1) {
-    res.sendFile(__dirname + '/build/app/html/index.html');
+    res.sendFile(`{__dirname}/build/app/html/index.html`);
   }
   else {
     next();
   }
 });
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.status(404);
   res.send('Page not found');
 });
 
-var port = Number(process.env.PORT || 8060);
+const port = Number(process.env.PORT || 8060);
 app.listen(port);
 console.log("docs server running on http://localhost:" + port);
