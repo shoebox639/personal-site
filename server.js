b42d5ffb4c52707bab75b1e6ad3767073ef80479
Shu Song <shoebox@gmail.com>
2016-04-04 00:59:29 -0400

    fixing syntax

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const app = express();
 
 app.use(compression());
 
-app.use('/assets/node_modules', express.static(`{__dirname}/node_modules`));
-app.use('/assets/bower_components', express.static(`{__dirname}/bower_components`));
-app.use('/assets', express.static(`{__dirname}/build`));
+app.use('/assets/node_modules', express.static(`${__dirname}/node_modules`));
+app.use('/assets/bower_components', express.static(`${__dirname}/bower_components`));
+app.use('/assets', express.static(`${__dirname}/build`));
 
 const validPaths = ['about', 'resume', 'contact'];
 
 app.get('/:path?', (req, res, next) => {
   const path = req.params.path;
   if (!path || validPaths.indexOf(path) > -1) {
-    res.sendFile(`{__dirname}/build/app/html/index.html`);
+    res.sendFile(`${__dirname}/build/app/html/index.html`);
   }
   else {
     next();
